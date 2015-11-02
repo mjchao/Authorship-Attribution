bd1a16a05f5345989984091bca18beab7666b6ad
Mickey Chao <mjchao2@gmail.com>
2015-11-02 10:59:41 -0500

    Create document abstractions

--- Document.py
+++ Document.py
@@ -4,26 +4,126 @@ Created on Nov 2, 2015
 @author: mjchao
 '''
 
+import re
+import string
+
+class DocumentType:
+    SAMPLE_STR = "sample"
+    TRAIN_STR = "train"
+    
+    SAMPLE = 0
+    TRAIN = 1
+    
+    '''
+    Determines the document type of a file - it is either "sample"
+    or "train" and receives an ID 0 or 1, respectively.
+    '''
+    @staticmethod
+    def getDocumentType( filename ):
+        if ( DocumentType.SAMPLE_STR in filename ):
+            return DocumentType.SAMPLE
+        elif ( DocumentType.TRAIN_STR in filename ):
+            return DocumentType.TRAIN
+
+'''
+Represents a word document.
+'''
 class Document( object ):
     
-    def __init__( self , authorId , text ):
+    '''
+    Creates a document object 
+    '''
+    def __init__( self , problemId , documentType , documentNumber , authorId , text ):
+        self._problemId = problemId
+        self._docType = documentType
+        self._docNum = documentNumber
         self._authorId = authorId
         self._text = text
         self._tokens = Document.tokenize( text )
-        
+       
+    '''
+    Returns a document object that represents the text document
+    with the associated directory and filename
+    ''' 
     @staticmethod
-    def createDocument( filename ):
-        pass
+    def createDocument( directory , fileWithExtension ):
+        
+        #remove the extension from the file to get the filename
+        filename = fileWithExtension[ 0 : fileWithExtension.find( "." ) ]
+        problemId = filename[ 0 ]
+        documentType = DocumentType.getDocumentType( filename )
+        
+        documentNumber = -1
+        authorId = -1
+        
+        #sample documents do not have an authorId
+        if ( documentType == DocumentType.SAMPLE ):
+            startOfNum = filename.find( DocumentType.SAMPLE_STR ) + len( DocumentType.SAMPLE_STR )
+            endOfNum = len( filename )
+            documentNumber = int( filename[ startOfNum : endOfNum ] )
+            
+        #training documents have author IDs followed by numbers
+        elif ( documentType == DocumentType.TRAIN ):
+            startOfAuthorId = filename.find( DocumentType.TRAIN_STR ) + len( DocumentType.TRAIN_STR )
+            endOfAuthorId = filename.find( "-" ) 
+            authorId = int( filename[ startOfAuthorId : endOfAuthorId ] )
+            
+            startOfDocumentNum = filename.find( "-" ) + 1
+            endOfDocumentNum = len( filename )
+            documentNumber = int( filename[ startOfDocumentNum : endOfDocumentNum ] )
+        
+        with open( directory + fileWithExtension , "r" ) as f:
+            content = f.read()
+            
+        return Document( problemId , documentType , documentNumber , authorId , content )
+
+    '''
+    Removes leading, trailing, and extra spaces from a string
+    '''
+    @staticmethod
+    def remove_extra_space( input_string ):
+        return re.sub("\s+", " ", input_string.strip())
     
+    '''
+    Converts some text into a list of tokens
+    '''
     @staticmethod
     def tokenize( text ):
-        pass
+        extra_space_removed = Document.remove_extra_space( text )
+        punctuation_removed = "".join([x for x in extra_space_removed if x not in string.punctuation])
+        lowercased = punctuation_removed.lower()
+        return lowercased.split()
+    
+    '''
+    Reports the textual representation of this document for debugging purposes. 
+    Only the first 10 tokens of the text will be shown to limit output size.
+    '''
+    def __str__( self ):
+        initialText = ""
+        for i in range( 0 , min(10 , len(self._tokens)) ):
+            initialText += self._tokens[ i ] + " "
+            
+        if ( len( self._tokens ) > 10 ):
+            initialText = "{" + initialText.strip() + " ... }"
+        else :
+            initialText = "{" + initialText.strip() + "}"
+            
+        return "Document[ ProblemId=" + str( self._problemId ) + \
+            " , DocumentType=" + str( self._docType ) + \
+            " , DocumentNumber=" + str( self._docNum ) + \
+            " , AuthorId=" + str( self._authorId ) + \
+            " , Text=" + str( initialText ) +\
+            " ]" 
     
     
 '''
 Unit testing
 '''
 def main():
+    doc1 = Document.createDocument( "problemset/problemA/" , "Asample02.txt" )
+    doc2 = Document.createDocument( "problemset/problemA/" , "Atrain01-1.txt" )
+    print doc1
+    print doc2
     pass
 
 if __name__ == "__main__" : main()
